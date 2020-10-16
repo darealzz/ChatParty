f8e9898fe143d90de7315f2f0ff49607cb35e80c
darealzz <58663214+darealzz@users.noreply.github.com>
2020-10-16 22:59:35 +0100

    Commit

--- ChatParty/settings.py
+++ ChatParty/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'Chatparty.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ChatParty',
+        'USER': 'postgres',
+        'PASSWORD': 'R^8#9*A"+5znaZ8L',
+        'HOST': '192.168.1.156'
     }
 }
+
 
 
 # Password validation
--- Main/views.py
+++ Main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,11 +13,35 @@ def index(request):
         return redirect('/login')
 
 def home(request):
+    if request.user.is_authenticated == True: #If they are logged in, send them to the home page. Else to the login page.
+        pass
+    else:
+        return redirect('/login')
+
 
     ctx = {'Title': 'Home'}
     return render(request, 'home.html', ctx)
 
 def login(request):
+    if request.user.is_authenticated == True: #If they are logged in, send them to the home page. Else to the login page.
+        return redirect('/home')
+    else:
+        pass
 
-    ctx = {'Title': 'Login'}
-    return render(request, 'login.html', ctx)
+
+    if request.method == 'POST':
+        username = request.POST['Username']
+        password = request.POST['Password']
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            auth_login(request, user)
+            return redirect('/home/')
+        else:
+            print(User.objects.get(username=username))
+            return redirect('/') 
+
+    else:
+        ctx = {'Title': 'Login'}
+        return render(request, 'login.html', ctx)
+
