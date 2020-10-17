2e7ed6479762850abb69c18104f25fc23aabe174
smars-h <69462678+smars-h@users.noreply.github.com>
2020-10-18 01:30:42 +0200

    Create utilities.py

---
+++ Main/utilities.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, aut
+from django.contrib.auth import authenticate, login
+from requests import request 
+
+class Utilities:
